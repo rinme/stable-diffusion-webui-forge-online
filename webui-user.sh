a09173f5cbcb73f55e1d7adc39f2bed59642f6a3
りんめ <rinmesk@yahoo.com>
2024-04-04 04:24:05 +0700

    chore: improve speed for a4000

--- webui-user.sh
+++ webui-user.sh
@@ -10,7 +10,7 @@
 #clone_dir="stable-diffusion-webui"
 
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-export COMMANDLINE_ARGS="--xformers --share"
+export COMMANDLINE_ARGS="--xformers --share --pin-shared-memory --cuda-malloc --cuda-stream"
 
 # python3 executable
 python_cmd="python"
